3138870a1505ab3dddc671146108bad707e398a9
DavidBonvin <ledave.musique@gmail.com>
2023-06-09 20:29:52 +0200

    meta

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import { ProjectItem } from './ProjectItem'
+import React from 'react'
 
 export const Projects = () => {
   return (
    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 '>
    <h1 className='text-4xl font-bold text-center mb-4 text-[#001b5e]'>Projects</h1>
    <p className='text-center py-8'>
-    Este projecto se trata de una app en microservicios, es un red social, se llama Tree-up, todavia no esta en la web, pero espero que pronto lo este. 
+    
    </p>
    <div className=' grid sm:grid-cols-2 gap-12'>
     <ProjectItem img='https://i.imgur.com/KNloxEO.png' title='Porfolio #1' url='https://davidbonvin.github.io/David-Bonvin/'/>
